f2ee3380cbe149abf9e30e99ccdfd472415845df
McRais <mcrais21@gmail.com>
2024-01-11 12:47:41 +0300

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -13,7 +13,7 @@ function resolutionValidator(array:any) {
 function isIsoDate(str:string) {                                        // validate the Date
     if (!/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(str)) return false;
     const d = new Date(str);
-    return !isNaN(d.getTime()) && d.toISOString()===str;
+    return d instanceof Date && !isNaN(d.getTime()) && d.toISOString()===str;
 }
 
  type VideoType = {
